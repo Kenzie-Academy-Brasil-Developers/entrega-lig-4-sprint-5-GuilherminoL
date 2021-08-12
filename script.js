cbf3a74676271b5746d02236a2aa55052ff2b762
gustavool1 <64279418+gustavool1@users.noreply.github.com>
2021-08-12 19:31:09 -0300

    fix: deleting console.log

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const updatingGame = (event) =>{
     if(event.target.classList.contains('column')){
 
       let column = event.target
-      console.log(column)
+     
       let rowOfGame = column.dataset.column - 1;
       let lastIndex = game[rowOfGame].lastIndexOf(0);
       if (lastIndex !== -1) {
@@ -167,7 +167,7 @@ const updatingGame = (event) =>{
       }
     }if(event.target.classList.contains("emptyCircle")){
       let column = event.target.parentElement
-      console.log(column)
+      
       let rowOfGame = column.dataset.column - 1;
       let lastIndex = game[rowOfGame].lastIndexOf(0);
       if (lastIndex !== -1) {
